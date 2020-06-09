6ee67503c2464e25ae3053d833b91584af023104
Bada1208 <sysoievbogdan@gmail.com>
2020-06-09 18:48:11 +0200

    added sorted set mapping

---
+++ 2.1d_collection_mapping_sortedset/src/main/java/com/sysoiev/mapping/sorted_set/Project.java
@@ -0,0 +1,77 @@
+package com.sysoiev.mapping.sorted_set;
+
+public class Project implements Comparable<Project> {
+    private int id;
+    private String projectName;
+    private String companyName;
+
+    /**
+     * Constructors
+     */
+    public Project() {
+    }
+
+    public Project(String projectName, String companyName) {
+        this.projectName = projectName;
+        this.companyName = companyName;
+    }
+
+
+    /**
+     * Getters and Setters
+     */
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Project:\n" +
+                "id: " + id +
+                "\nProject Name: " + projectName +
+                "\nCompany Name: " + companyName + "\n";
+    }
+
+    @Override
+    public int compareTo(Project that) {
+        final int BEFORE = -1;
+        final int AFTER = 1;
+
+        if (that == null) {
+            return BEFORE;
+        }
+
+        Comparable thisProject = this.getProjectName();
+        Comparable thatProject = that.getProjectName();
+
+        if (thisProject == null) {
+            return AFTER;
+        } else if (thatProject == null) {
+            return BEFORE;
+        } else {
+            return thisProject.compareTo(thatProject);
+        }
+    }
+}
